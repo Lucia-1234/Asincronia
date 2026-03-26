3897d04d5998746901a2ae4651fc15fed1dd230c
Lucia <Lulizcano.aa@hotmail.com>
2026-03-26 07:34:21 -0500

    feat(async): implementar ejercicio de asincronia basica con setTimeout (#1)

---
+++ src/app.js
@@ -0,0 +1,9 @@
+//imprime inicio 
+console.log("Inicio");
+
+//operacion con setTimeout que tarda 2 segundos y fin
+setTimeout(() => {
+    console.log("Despues de 2 segundos");
+    console.log("Fin")
+}, 2000);
+
